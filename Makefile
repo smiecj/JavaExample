63824dc74bc83a58ed98299f38f0e1f596135eb0
lifeng <459559238@qq.com>
2021-09-17 14:39:34 +0800

    feature-add encode test method; clear gradle config, only keep maven config

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 run:
+	ps -ef | grep "rest-service-complete" | grep -v grep | awk '{print $$2}' | xargs --no-run-if-empty kill -9
 	mvn clean package -DskipTests
-	java -Dserver.port=8081 -jar target/rest-service-complete-0.0.1-SNAPSHOT.jar
+	nohup java -Dserver.port=8081 -jar target/rest-service-complete-0.0.1-SNAPSHOT.jar > test.log 2>&1 &
 
-test_command:
+exec:
 	curl http://localhost:8081/command/exec
+
+encode:
+	curl http://localhost:8081/command/encode
--- src/main/java/com/basic/command/ProcessExecutorController.java
+++ src/main/java/com/basic/command/ProcessExecutorController.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,4 +40,12 @@ public class ProcessExecutorController {
         }
         return 0;
 	}
+
+    @GetMapping("/encode")
+    public String encode() {
+        logger.warn("System default encoding: " + System.getProperty("file.encoding"));
+        String encodeStr = new String(Base64.encodeBase64("test".getBytes()), StandardCharsets.UTF_8);
+        System.out.println(encodeStr);
+        return encodeStr;
+    }
 }
